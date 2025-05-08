f55c006306996da64058a0693bdb91813f972ac1
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-05-08 15:13:47 +0100

    Added api key for api key authentication on the server

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
-
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const api = axios.create({
     // baseURL: 'http://localhost:2000/api',
     baseURL: 'https://sportmonksapi.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
+        "X-API-KEY": API_KEY
     },
 })
 
